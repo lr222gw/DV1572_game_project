f2c182b1fc6e419a2d97d8e830f6a0fc87cd2d17
falkgaard <falkgaard@gmail.com>
2019-01-27 17:42:52 +0100

    Made some tweaks to defs.h to include GL libraries and give them some aliases

--- src/misc/defs.h
+++ src/misc/defs.h
@@ -25,6 +25,11 @@
 
 #include "enum.h"
 
+#include <GL/glew.h> // <- always include before gl.h & glfw3.h
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 // #include <string_view>
 
 // unsigned integer types:
@@ -37,29 +42,32 @@
                             using Int16        = int16_t;
                             using Int32        = int32_t;
                             using Int64        = int64_t;
+// glm vectors:
+                            using Vec2         = glm::vec2;
+                            using Vec3         = glm::vec3;
+                            using Vec4         = glm::vec4;
+// glm matrices:
+                            using Mat2         = glm::mat2;
+                            using Mat3         = glm::mat3;
+                            using Mat4         = glm::mat4;
 // floating point types:
                             using Float32      = float;
                             using Float64      = double;
 // string types:
                             using String       = std::basic_string<char>;
-                            using Str          = String;
                             using StringView   = std::basic_string_view<char>;
-                            using StrView      = StringView;
 // stream types:
                             using Ifstream     = std::ifstream;
                             using StringStream = std::stringstream;
 // index type:
                             using Index        = int;
-                            using Idx          = Index;
 // size type:
                             using Size         = size_t;
 // boolean type:
                             using Bool         = bool;
 // array types:
 template <class T>          using Vector       = std::vector<T>;
-template <class T>          using Vec          = Vector<T>;
 template <class T, Size N>  using Array        = std::array<T,N>;
-template <class T, Size N>  using Arr          = Array<T,N>;
 // smart pointer types:
 template <class T>          using SharedPtr    = std::shared_ptr<T>;
 template <class T>          using UniquePtr    = std::unique_ptr<T>;
@@ -69,7 +77,6 @@ template <class K, class V> using Map          = std::map<K,V>;
 template <class K, class V> using HashMap      = std::unordered_map<K,V>;
 // optional type:
 template <class T>          using Optional     = std::optional<T>;
-template <class T>          using Opt          = Optional<T>;
 
 constexpr Idx  const   invalid_idx = -1;
 constexpr char const * invalid_str = "ERROR";
