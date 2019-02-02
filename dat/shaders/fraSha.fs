0860adc2b704c02e9e28d19ff7be1f5185d7084b
falkgaard <falkgaard@gmail.com>
2019-02-02 01:59:54 +0100

    Small shader tweak

--- dat/shaders/fraSha.fs
+++ dat/shaders/fraSha.fs
@@ -1,15 +1,14 @@
 #version 440 core
 
 out vec4 FragColor;
-
-in vec2 uv_fs;
+in  vec2 uv_fs;
 
 uniform sampler2D tex_diff1;
 uniform sampler2D tex_spec1;
 uniform sampler2D tex_norm1;
 
-void main()
-{    
-    FragColor = texture(tex_diff1, uv_fs) + texture(tex_spec1, uv_fs) + texture(tex_norm1, uv_fs);
+void main() {    
+    FragColor = texture( tex_diff1, uv_fs )
+              + texture( tex_spec1, uv_fs )
+              + texture( tex_norm1, uv_fs );
 }
-
