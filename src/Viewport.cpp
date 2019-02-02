a7a0a144d620af5c6cf0e1e66e1561980b7e970f
falkgaard <falkgaard@gmail.com>
2019-02-02 03:34:32 +0100

    Tweaks to Viewport.cpp

--- src/Viewport.cpp
+++ src/Viewport.cpp
@@ -8,16 +8,18 @@ Viewport::Viewport(Vec3 position , Vec3 rotation, Float32 fov) :
 
    _model = Mat4(1.0f);
 
+   // validera
    _view = glm::lookAt( position,
                         Vec3{ 0, 0,  0 },
                         Vec3{ 0, 1,  0 } );
 
+   // validera
    _projection = glm::perspective( fov,
                                    config::aspect_ratio,
                                    config::near_plane,
                                    config::far_plane);
    
-   //tillfï¿½llig
+   //tillfällig
    // _model = glm::rotate(_model, glm::radians(32.0f), Vec3(0.0f, 1.0f, 0.0f));
    
    _write_to_buffer();
@@ -35,7 +37,7 @@ void Viewport::set_rotation(Vec3 new_rotation) {
 }
 
 void Viewport::transform(Mat4 transformation) {
-   _model *= transformation;
+   _model *= transformation; // validera
    _write_to_buffer();
 }
 
@@ -44,7 +46,8 @@ void Viewport::bind_shader_program(ShaderProgram &shapro) {
    _write_to_buffer();
 }
 
-void Viewport::_update_view_matrix() {
+void Viewport::_update_model_matrix() {
+   // validera?
    Mat4 new_position       = Mat4(1.0f);
         new_position[0][3] = _position.x;
         new_position[1][3] = _position.y;
@@ -52,12 +55,13 @@ void Viewport::_update_view_matrix() {
 
    glm::quat rotation_quaternion {Vec4( _rotation, 0.0f) };
 
-   _view = new_position * glm::toMat4(rotation_quaternion);;
+   _model = new_position * glm::toMat4(rotation_quaternion);;
 
    _write_to_buffer();
 }
 
 
 void Viewport::_write_to_buffer() {
+   _camera = _projection * _view * _model; // validera?
    glUniformMatrix4fv(_location, 1, GL_FALSE, glm::value_ptr(_camera));
 }
