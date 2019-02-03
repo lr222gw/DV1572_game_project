15cef19f595e6d33b9445dd66ea7072b40e79f2e
falkgaard <falkgaard@gmail.com>
2019-02-03 22:02:26 +0100

    Uncommented model_location

--- src/ModelInstance.cpp
+++ src/ModelInstance.cpp
@@ -2,7 +2,7 @@
 
 void ModelInstance::draw() {
    // för över _transform till shader programmet
-   glUniformMatrix4fv(_shader_program->get_transform_location(), 1, GL_FALSE, glm::value_ptr(_transform));
+   //glUniformMatrix4fv(_shader_program->get_transform_location(), 1, GL_FALSE, glm::value_ptr(_transform));
    _model->draw(*_shader_program);  // för över vertriser osv till gpu
 }
 
