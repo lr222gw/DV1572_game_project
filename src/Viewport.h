4f97955c78d5c96f36af6ca521d6ef7d551eac46
falkgaard <falkgaard@gmail.com>
2019-02-04 20:42:22 +0100

    Added Transform to Viewport

--- src/Viewport.h
+++ src/Viewport.h
@@ -9,25 +9,29 @@ class Viewport {
 public:
    Viewport(Vec3 position, Float32 fov = config::fov_rad);
 
-   void transform(Mat4 transformation);
+   void transform( Transform const &transform );
+   void set_transform( Transform const &transform );
 
    void bind_shader_program(ShaderProgram &shapro);
 
-   void set_position(Vec3 new_position);
-
-   void set_rotation(Vec3 new_rotation);
+   // void set_position(Vec3 new_position);
+   // void set_rotation(Vec3 new_rotation);
 
    void set_fov(Float32 fov_rad);
 
 
 private:
-   void _update_view_matrix();
+   // void _update_view_matrix();
    void _write_to_buffer();
 
-   Vec3 _position,
-        _rotation;
+   // Vec3 _position,
+   //      _rotation;
 
    Float32 _fov;
-   Mat4 _view, _projection, _model;
+
+   Transform _model;
+   Mat4 _projection, _view;
+   // TODO: (låg prioritet) använd transform för projection?
+
    GLuint _location;
 };
