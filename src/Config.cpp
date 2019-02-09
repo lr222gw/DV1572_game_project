4c40dfe6310d095a43d4670db2f1b8bd17fffe86
falkgaard <falkgaard@gmail.com>
2019-02-09 01:57:55 +0100

    Config bug fix, attempt 2

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,3 +1,7 @@
 #include "Config.h"
 
+String const Config::shader_path  { "dat/shaders/" };
+String const Config::model_path   { "dat/models/"  };
+String const Config::texture_path { "dat/models/"  }; //TODO: unneeded
+
 Config config {}; // <- definition of global config instance
